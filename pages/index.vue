22c91abed1c5d3cd6931c955ef00830a5a2b05fa
Artur Frank <frank.artur2005@gmail.com>
2024-10-29 16:27:48 +0500

    add test image on page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    Pudge
-  </div>
+  <img src="https://preview.redd.it/what-rarity-will-miyabi-v0-d6678sypamad1.jpeg?width=1080&crop=smart&auto=webp&s=503893bd92507be1416da984dfcfc61e357b2880" />
 </template>
